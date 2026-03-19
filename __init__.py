189a9af88b075dfd0966e0dc933070f8cb02470a
koketasi <5648souma@gmail.com>
2026-03-19 15:49:20 +0900

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-
-
-#database='database.db'
-
-
-#with sqlite3.connect(database) as con:
-   # con.execute('CREATE TABLE IF NOT EXISTS schedule(id INTEGER PRIMARY KEY AUTOINCREMENT, year TEXT,month TEXT,day TEXT, hour TEXT,minute TEXT,event TEXT, file_name TEXT, file_title TEXT)')
-  #  con.commit()
