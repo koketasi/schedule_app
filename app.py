565f4454723c0770727e6751084e1700de959746
koketasi <5648souma@gmail.com>
2026-04-05 14:01:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def index():
            #         con.execute('UPDATE schedule SET year=?,month=?,day=?,hour=?,minute=?,event=?,file_name=?,file_title=? WHERE rowid=?',[year,month,day,hour,miute,event,current_name,file_title,row])
             #        con.commit()
                 supabase.table('schedule').update({
-                    'year':year,'month':month,'day':day,'hour':hour,'minute':miute,'event':event,'current_name':file_name,'file_title':file_title
+                    'year':year,'month':month,'day':day,'hour':hour,'minute':miute,'event':event,'file_name':current_name,'file_title':file_title
                 }).eq('id',int(row)).execute()
 
                 return redirect(url_for('index'))
